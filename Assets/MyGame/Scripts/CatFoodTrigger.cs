e8e48362717803a1dd7e0544d5fbed5062712fe2
Eva Innerlohninger <evainnerlohninger@MacBook-Pro-M1-von-Eva.local>
2021-06-16 14:46:05 +0200

    Cat Trigger works and normal map added on Bush labyrinth

---
+++ Assets/MyGame/Scripts/CatFoodTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatFoodTrigger : MonoBehaviour
+{
+    public GameObject catCol;
+    public GameObject Coin2;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (catCol.CompareTag("CatTrigger"))
+        {
+            Coin2.transform.position= new Vector3(7, 1, 11);
+            transform.position = new Vector3(7, 5, 13);
+        }
+
+    }
+}
